5dcbda3834d2ab9361440abf0459f4570640345c
DungVC-Dev <dungvc.dev@gmail.com>
2023-10-25 17:01:57 +0700

    Login view

---
+++ GuardSystemApp/Shared/CommonView/ButtonStateCommon.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonStateCommon.swift
+//  GuardSystemApp
+//
+//  Created by dungvc1 on 25/10/2023.
+//
+
+import SwiftUI
+
+struct ButtonStateCommon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonStateCommon_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonStateCommon()
+    }
+}
---
+++ GuardSystemApp/Shared/CommonView/TextFieldCommon.swift
@@ -0,0 +1,20 @@
+//
+//  TextFieldCommon.swift
+//  GuardSystemApp
+//
+//  Created by dungvc1 on 25/10/2023.
+//
+
+import SwiftUI
+
+struct TextFieldCommon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextFieldCommon_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldCommon()
+    }
+}
