6b1880dad781bac959b3791b2c8b46eb26cca27b
FPT-DungVC1 <dungvc1@fsoft.com>
2023-10-26 19:31:00 +0700

    loginview

--- GuardSystemApp/Modules/RepoList/Login/LoginViewModel.swift
+++ GuardSystemApp/Modules/RepoList/Login/LoginViewModel.swift
@@ -13,7 +13,6 @@ class LoginViewModel: ObservableObject {
     @Published public var alert: AlertViewObject? = nil
     private var cancellables = Set<AnyCancellable>()
     var loginProvider: LoginProvider = LoginClient()
-    var netWorkError: NetworkError = .invalidInput
 
     func login(username: String, password: String) {
         let request = LoginRequest(username: username, passwd: password)
@@ -21,9 +20,8 @@ class LoginViewModel: ObservableObject {
             .sink(receiveCompletion: { completion in
                 switch completion {
                 case .failure(let error):
-                    self.netWorkError = error as! NetworkError
                     self.alert = AlertViewObject(
-                        title: netWorkError,
+                        title: "Error",
                         message: "An error occurred: \(error)",
                         titleSecondary: "OK"
                     )
